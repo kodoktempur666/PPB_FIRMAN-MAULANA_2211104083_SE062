c23afeb029a93b9217da8ff6cd095b9cf34d514d
Firman Maulana <147988428+kodoktempur666@users.noreply.github.com>
2024-10-02 23:24:05 +0700

    Update fungsi.dart

--- 03_Pengenalan_Dart/GUIDED/fungsi.dart
+++ 03_Pengenalan_Dart/GUIDED/fungsi.dart
@@ -1,1 +1,14 @@
+void cetakPesan(String pesan) {
+  print(pesan); // Mencetak pesan
+}
 
+int perkalian(int a, int b) {
+  return a * b; // Mengembalikan nilai perkalian
+}
+
+void main () {
+  int hasil = perkalian(5, 10);
+  print("Hasil perkalian: $hasil");
+
+  cetakPesan("Kamu Pintar");
+}
