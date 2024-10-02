39eed36cadafd47ff9a605e4f2713e06b508abc2
Firman Maulana <147988428+kodoktempur666@users.noreply.github.com>
2024-10-02 23:25:12 +0700

    guided

---
+++ 03_Pengenalan_Dart/GUIDED/variabel.dart
@@ -0,0 +1,13 @@
+void main() {
+  var name = "Irpan";
+  var age = "20";
+  print("name: $name, usia: $age");
+
+  // pengaman variabel menggunakan camelCase
+  String firstName, lastName; // Tipe data String
+  firstName = "Irpan";
+  lastName = "Muria";
+  final cuaca = "Hujan";
+
+  print("$firstName $lastName - Cuaca: $cuaca");
+}
