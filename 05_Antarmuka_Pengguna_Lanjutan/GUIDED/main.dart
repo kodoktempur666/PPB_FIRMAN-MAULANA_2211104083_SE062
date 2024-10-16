3e1af935a56b3e08f8a012a71df87832890a3889
Firman Maulana <147988428+kodoktempur666@users.noreply.github.com>
2024-10-16 19:15:45 +0700

    laprak 5

--- 05_Antarmuka_Pengguna_Lanjutan/GUIDED/main.dart
+++ 05_Antarmuka_Pengguna_Lanjutan/GUIDED/main.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:guided/custom.dart';
+import 'package:guided/jenis_list_view.dart';
+import 'package:guided/flexble_expanded.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: JenisListView(),
+      // home: FlexibleExpandedScreen(),
+      // home: Custom(),
+    );
+  }
+}
