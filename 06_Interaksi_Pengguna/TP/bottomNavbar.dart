a385a1e76e2547f4dddde3d322b9b835c0ed60b4
Firman Maulana <147988428+kodoktempur666@users.noreply.github.com>
2024-10-21 22:55:53 +0700

    laprak 6

--- 06_Interaksi_Pengguna/TP/bottomNavbar.dart
+++ 06_Interaksi_Pengguna/TP/bottomNavbar.dart
@@ -1,1 +1,55 @@
+import 'package:flutter/material.dart';
 
+class BottomNavBarExample extends StatefulWidget {
+  @override
+  _BottomNavBarExampleState createState() => _BottomNavBarExampleState();
+}
+
+class _BottomNavBarExampleState extends State<BottomNavBarExample> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Ini adalah halaman Home', style: TextStyle(fontSize: 24)),
+    Text('Ini adalah halaman Wisata', style: TextStyle(fontSize: 24)),
+    Text('Ini adalah halaman Profil', style: TextStyle(fontSize: 24)),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bottom Navigation Bar Example'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.place),
+            label: 'Wisata',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profil',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white, // Warna item yang dipilih
+        unselectedItemColor: Colors.black, // Warna item yang tidak dipilih
+        backgroundColor: const Color.fromARGB(255, 198, 0, 248), // Warna background BottomNavigationBar
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
